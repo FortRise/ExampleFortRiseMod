dc19af04b48bfb509b8ea90813920e5b164fc711
Terria028 <86240437+Terria-K@users.noreply.github.com>
2023-08-30 23:21:35 +0800

    Update Bartizan

--- BartizanMod/Core/ModImport/EightPlayerMod.cs
+++ BartizanMod/Core/ModImport/EightPlayerMod.cs
@@ -1,3 +1,4 @@
+using System;
 using MonoMod.ModInterop;
 
 namespace BartizanMod;
@@ -8,4 +9,22 @@ public class EightPlayerImport
 {
     public static Func<bool> IsEightPlayer;
     public static Func<bool> LaunchedEightPlayer;
+}
+
+public static class EightPlayerUtils 
+{
+    public static int GetScreenWidth() 
+    {
+        return BartizanModModule.EightPlayerMod ? EightPlayerImport.IsEightPlayer() ? 420 : 320 : 320;
+    }
+
+    public static int GetPlayerCount() 
+    {
+        return BartizanModModule.EightPlayerMod ? EightPlayerImport.IsEightPlayer() ? 8 : 4 : 4;
+    }
+
+    public static int GetMenuPlayerCount() 
+    {
+        return BartizanModModule.EightPlayerMod ? EightPlayerImport.LaunchedEightPlayer() ? 8 : 4 : 4;
+    }
 }
