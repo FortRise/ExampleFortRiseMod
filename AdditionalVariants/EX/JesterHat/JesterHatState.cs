e1323965745eef66040b609746f8ca2836daf350
FCornaire <cornairemax@yahoo.fr>
2023-10-13 23:44:26 +0200

    feat: add EX mod interop with jester hat variant

---
+++ AdditionalVariants/EX/JesterHat/JesterHatState.cs
@@ -0,0 +1,13 @@
+﻿namespace AdditionalVariants.EX.JesterHat
+{
+    internal class JesterHatState
+    {
+        public Dictionary<int, IEnumerable<Vector2f>> JesterHatsWarpPoints { get; set; } = new Dictionary<int, IEnumerable<Vector2f>>();
+    }
+
+    public class Vector2f
+    {
+        public float X { get; set; }
+        public float Y { get; set; }
+    }
+}
---
+++ AdditionalVariants/EX/ModImports.cs
@@ -0,0 +1,16 @@
+﻿using FortRise;
+using MonoMod.ModInterop;
+
+namespace AdditionalVariants.EX
+{
+    [ModImportName("TF.EX.API")]
+    public static class TfExAPIModImports
+    {
+        public static Action<FortModule, string, Func<string>, Action<string>> RegisterVariantStateEvents;
+
+        static TfExAPIModImports()
+        {
+            typeof(TfExAPIModImports).ModInterop();
+        }
+    }
+}
