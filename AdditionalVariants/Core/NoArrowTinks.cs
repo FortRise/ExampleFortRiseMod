29a1712659926f322947bccef05089f00eac7ab4
Terria-K <teuria@proton.me>
2025-01-31 17:54:39 +0800

    Fixed arrow tinks on windows

--- AdditionalVariants/Core/NoArrowTinks.cs
+++ AdditionalVariants/Core/NoArrowTinks.cs
@@ -20,8 +20,10 @@ public static class NoArrowTinks
     {
         var cursor = new ILCursor(il);
 
+        int ldlocIndex = RiseCore.IsWindows ? 3 : 2;
+
         if (cursor.TryGotoNext(MoveType.After, 
-            instr => instr.MatchLdloc(2),
+            instr => instr.MatchLdloc(ldlocIndex),
             instr => instr.MatchCallOrCallvirt<Arrow>("get_Dangerous")))
         {
             cursor.EmitDelegate((bool isDangerous) => isDangerous && !VariantManager.GetCustomVariant("NoArrowTinks"));
